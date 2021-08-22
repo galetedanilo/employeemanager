40db5622a31a1960d696810261a4468b6324238e
galetedanilo <galetedanilo@gmail.com>
2021-08-22 20:32:08 -0300

    Create: Employee Model in entities pack

---
+++ backend/src/main/java/com/galete/employeemanager/entities/Employee.java
@@ -0,0 +1,38 @@
+package com.galete.employeemanager.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Employee implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(nullable = false, updatable = false)
+	private Long employeeId;
+	private String name;
+	private String email;
+	private String jobTitle;
+	private String phone;
+	private String imageUrl;
+	@Column(nullable = false, updatable = false)
+	private String employeeCode;
+}
