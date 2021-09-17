a63657382848a6b89e43567533f9f29a6e846cde
galetedanilo <galetedanilo@gmail.com>
2021-09-16 21:32:20 -0300

    Remove: Anotation column in id column
    Remove the anotition column in id column with the paramaters nullable
    and update

--- backend/src/main/java/com/galete/employeemanager/entities/Employee.java
+++ backend/src/main/java/com/galete/employeemanager/entities/Employee.java
@@ -32,7 +32,6 @@ public class Employee implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(nullable = false, updatable = false)
 	private Long id;
 	
 	@Column(nullable = false)
