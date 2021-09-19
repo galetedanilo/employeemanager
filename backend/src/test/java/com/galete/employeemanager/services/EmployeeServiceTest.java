82953c8f21725f74c3cfc4f19a7104657d56a721
galetedanilo <galetedanilo@gmail.com>
2021-09-19 15:33:22 -0300

    Create delete employee tests
    In this commit create delete employee test with returns void for
    existing id, ResourceNotFoundeException for does not existing id and
    DatabaseException for dependent id
    
    Ref #1

--- backend/src/test/java/com/galete/employeemanager/services/EmployeeServiceTest.java
+++ backend/src/test/java/com/galete/employeemanager/services/EmployeeServiceTest.java
@@ -11,6 +11,8 @@ import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -22,6 +24,7 @@ import com.galete.employeemanager.factories.EmployeeFactory;
 import com.galete.employeemanager.repositories.EmployeeRepository;
 import com.galete.employeemanager.requests.EmployeeRequest;
 import com.galete.employeemanager.responses.EmployeeResponse;
+import com.galete.employeemanager.services.exceptions.DatabaseException;
 import com.galete.employeemanager.services.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class)
@@ -41,11 +44,13 @@ public class EmployeeServiceTest {
 		
 	private Long existingId;
 	private Long nonExistingId;
+	private Long dependentId;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		existingId = 1L;
 		nonExistingId = 2L;
+		dependentId = 3L;
 		
 		employee = EmployeeFactory.createEmployee();
 		employeeRequest = EmployeeFactory.createEmployeeRequest();
@@ -54,8 +59,12 @@ public class EmployeeServiceTest {
 		
 		Mockito.when(employeeRepository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(employeePage);
 		
-		Mockito.when(employeeRepository.findById(existingId)).thenReturn(Optional.of(employee));
-		Mockito.when(employeeRepository.findById(nonExistingId)).thenReturn(Optional.empty());
+		Mockito.when(employeeRepository.findEmployeeById(existingId)).thenReturn(Optional.of(employee));
+		Mockito.when(employeeRepository.findEmployeeById(nonExistingId)).thenReturn(Optional.empty());
+		
+		Mockito.doNothing().when(employeeRepository).deleteById(existingId);
+		Mockito.doThrow(EmptyResultDataAccessException.class).when(employeeRepository).deleteById(nonExistingId);
+		Mockito.doThrow(DataIntegrityViolationException.class).when(employeeRepository).deleteById(dependentId);
 		
 		Mockito.when(employeeRepository.save(ArgumentMatchers.any())).thenReturn(employee);
 		
@@ -79,7 +88,7 @@ public class EmployeeServiceTest {
 		
 		Assertions.assertNotNull(employeeResponse);
 		
-		Mockito.verify(employeeRepository, Mockito.times(1)).findById(existingId);
+		Mockito.verify(employeeRepository, Mockito.times(1)).findEmployeeById(existingId);
 	}
 	
 	@Test
@@ -89,11 +98,42 @@ public class EmployeeServiceTest {
 			employeeService.findEmployeeById(nonExistingId);
 		});
 		
-		Mockito.verify(employeeRepository, Mockito.times(1)).findById(nonExistingId);
+		Mockito.verify(employeeRepository, Mockito.times(1)).findEmployeeById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteEmployeeShouldReturnNothingWhenIdExits() {
+		
+		Assertions.assertDoesNotThrow(() -> {
+			employeeService.deleteEmployee(existingId);
+		});
+		
+		Mockito.verify(employeeRepository, Mockito.times(1)).deleteById(existingId);
+	}
+	
+	@Test
+	public void deleteEmployeeShouldReturnThrowResourceNotFoundExceptionWhenIdDoesNotExisting() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			employeeService.deleteEmployee(nonExistingId);
+		});
+		
+		Mockito.verify(employeeRepository, Mockito.times(1)).deleteById(nonExistingId);
+	}
+	
+	@Test
+	public void deleteEmployeeShouldReturnThrowDatabaseExceptionWhenDepenentId() {
+		
+		Assertions.assertThrows(DatabaseException.class, () -> {
+			employeeService.deleteEmployee(dependentId);
+		});
+		
+		Mockito.verify(employeeRepository, Mockito.times(1)).deleteById(dependentId);
 	}
 	
 	@Test
 	public void addEmployeeShouldReturnEmployeeResponse() {
+		
 		EmployeeResponse employeeResponse = employeeService.addEmployee(employeeRequest);
 		
 		Assertions.assertNotNull(employeeResponse);
